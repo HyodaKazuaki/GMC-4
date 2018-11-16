718eb48797ebb1a40f8bf630ffc51411f221f2e8
Kazuaki Hyoda <kazuaki@ex-t.jp>
2018-11-16 16:14:44 +0900

    [Add]Added address class

---
+++ GMC-4/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMC_4
+{
+    static class Address
+    {
+        private static byte address = 0x00;
+        
+        public static byte getAddress()
+        {
+            return address;
+        }
+
+        public static void incrementAddress()
+        {
+            address = (address == 0x7F) ? (byte)0 : (byte)++address;
+        }
+
+        public static void decrementAddress()
+        {
+            address = (address == 0x00) ? (byte)0x7F : (byte)--address;
+        }
+
+        public static void setAddress(byte addr)
+        {
+            if (0x7F <= addr && addr >= 0x0)
+                address = addr;
+        }
+    }
+}
